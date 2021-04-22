16e6f3b4ce33c38cbeb70f091620c6f222aaf27e
JazInimicus <57916818+JazInimicus@users.noreply.github.com>
2021-04-22 21:17:08 +0200

    updated main

--- 1. Praktikum/main.cpp
+++ 1. Praktikum/main.cpp
@@ -19,21 +19,21 @@ int main()
 
 
 	//for Function f
-	CMyVektor x;
-
-	x.set_v(0, 0.2);
-	x.set_v(1, -2.1);
+	CMyVektor vector = CMyVektor();
+	vector.set_d(2);
+	vector.set_v(0, 0.2);
+	vector.set_v(1, -2.1);
 
 	gradientenverfahren(x, func, 1);
 
 
 	//for Function g
 
-	CMyVektor x;
-
-	x.set_v(0, 0);
-	x.set_v(1, 0);
-	x.set_v(2, 0);
+	CMyVektor vector;
+	vector.set_d(3);
+	vector.set_v(0, 0);
+	vector.set_v(1, 0);
+	vector.set_v(2, 0);
 
 	gradientenverfahren(x, func, 0.1);
 
